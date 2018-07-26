52347c2fb7c3082727ed5cca563e5fccc706f6c6
jianglongjian <2511067577@qq.com>
2018-07-26 09:46:59 +0800

    Initial Commit

---
+++ JXVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXVideoPlayer
+//
+//  Created by zl on 2018/7/26.
+//  Copyright © 2018年 longjianjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXVideoPlayer
+//
+//  Created by zl on 2018/7/26.
+//  Copyright © 2018年 longjianjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
